f33c770021db4aeba56ed4b5445dd4bd714a16bc
Andy Feetenby <andyfeetenby@gmail.com>
2022-07-05 10:50:22 +0700

    Updated ActorsService to inherit from Base Repository

--- Data/Base/EntityBaseRepository.cs
+++ Data/Base/EntityBaseRepository.cs
@@ -1,33 +1,50 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace eTickets.Data.Base
 {
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
-        public Task AddAsync(T entity)
+        private readonly AppDbContext _context;
+
+        public EntityBaseRepository(AppDbContext context)
         {
-            throw new System.NotImplementedException();
+            _context = context;
+        }
+        
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
         }
 
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var entity = await _context.Set<T>().FirstOrDefaultAsync( n => n.Id == id);
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<T>> GetAllAsync()
+        public async Task<IEnumerable<T>> GetAllAsync()
         {
-            throw new System.NotImplementedException();
+            var result = await _context.Set<T>().ToListAsync();
+            return result;
         }
 
-        public Task<T> GetByIdAsync(int id)
+        public async Task<T> GetByIdAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var result = await _context.Set<T>().FirstOrDefaultAsync( n => n.Id == id);
+            return result;
         }
 
-        public Task<T> UpdateAsync(int id, T entity)
+        public async Task UpdateAsync(int id, T entity)
         {
-            throw new System.NotImplementedException();
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Modified;
+            await _context.SaveChangesAsync();
         }
     }
 }
--- Data/Services/IActorsService.cs
+++ Data/Services/IActorsService.cs
@@ -1,19 +1,12 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using eTickets.Data.Base;
 using eTickets.Models;
 
 namespace eTickets.Data.Services
 {
-    public interface IActorsService
+    public interface IActorsService: IEntityBaseRepository<Actor>
     {
-        Task<IEnumerable<Actor>> GetAllAsync();
-
-        Task<Actor> GetByIdAsync(int id);
-
-        Task AddAsync(Actor actor);
-
-        Task<Actor> UpdateAsync(int id, Actor newActor);
-
-        Task DeleteAsync(int id);
+       
     }
 }
