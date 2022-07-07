431f24bc931ca7818d2d12294b97c0aa3f53ccb3
Andy Feetenby <andyfeetenby@gmail.com>
2022-07-07 21:21:36 +0700

    Added ShoppingCart class and ShoppingCartItem Model

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -46,5 +46,7 @@ namespace eTickets.Data
         // Orders Related Tables
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet <ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
