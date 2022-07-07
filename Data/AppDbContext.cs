8d7e60a893662867eeace090e166408a3cc6aad5
Andy Feetenby <andyfeetenby@gmail.com>
2022-07-07 21:05:43 +0700

    Added Order and OrderItem Models

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -42,5 +42,9 @@ namespace eTickets.Data
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
+
+        // Orders Related Tables
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
