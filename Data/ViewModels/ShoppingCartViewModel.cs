d853ccf9fdb96203cef0891df8e9a1b9feef2f80
Andy Feetenby <andyfeetenby@gmail.com>
2022-07-08 10:50:32 +0700

    Setup OrdersController and ShoppingCart ViewModel

---
+++ Data/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,10 @@
+using eTickets.Data.Cart;
+
+namespace eTickets.Data.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
