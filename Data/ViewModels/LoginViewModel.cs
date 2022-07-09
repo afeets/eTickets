f7a480abdb4721f77f55f6146a88361048c70ee9
Andy Feetenby <andyfeetenby@gmail.com>
2022-07-09 14:55:48 +0700

    Added Login ViewModel and View

---
+++ Data/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Data.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email Address is required")]
+        public string EmailAddress { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
